b0365a1347074e02d7918bdceb27eb4bd411f8ba
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-12-12 19:22:44 -0800

    finished rock, paper, scissors.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,11 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  if n == 1: return 3
-  else:
-    return n * rock_paper_scissors(n-1)
 
-rock_paper_scissors(4)
+  options = ['rock', 'paper', 'scissors']
+  output = []
+
+  if n == 0:
+    return [[]]
+  elif n == 1: 
+    return [[options[0]]] + [[options[1]]] + [[options[2]]]
+  else:
+    r1 = rock_paper_scissors(n-1)
+    for spot in r1:
+      output.append(spot + [options[0]])
+      output.append(spot + [options[1]])
+      output.append(spot + [options[2]])
+  return output
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
